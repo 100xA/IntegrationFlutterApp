330042fd09b0ad2e4c199dd6bd4dc72874ad11ca
100xA <biernackijan4@gmail.com>
2023-07-11 08:56:35 +0200

    Profile

--- lib/components/profile/ui/profile_screen.dart
+++ lib/components/profile/ui/profile_screen.dart
@@ -29,7 +29,8 @@ class ProfileScreen extends StatelessWidget {
             ),
             const IntegrationCustomAvatar(
                 radius: 100,
-                url: "https://pbs.twimg.com/media/EhcyEQAX0AA2PwP.jpg"),
+                url:
+                    "https://media.licdn.com/dms/image/D4E03AQHgf5CKvuUH8w/profile-displayphoto-shrink_800_800/0/1687419413255?e=1694649600&v=beta&t=GREcvaPziLp_0RiDq3ZCteIGzO5dBqCyHJ4whJxydno"),
             const SizedBox(
               height: 20,
             ),
@@ -45,11 +46,17 @@ class ProfileScreen extends StatelessWidget {
                 ),
                 Text(
                   state.profile?.name ?? "Kein name gefunden",
-                  style: const TextStyle(fontSize: 20),
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
                 Text(
                   profileCubit.jobTypeFilter(state.profile?.job),
-                  style: const TextStyle(fontSize: 20),
+                  style: const TextStyle(
+                      fontSize: 20,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
                 ),
                 const SizedBox(
                   height: 20,
@@ -60,24 +67,71 @@ class ProfileScreen extends StatelessWidget {
                       borderRadius: BorderRadius.all(Radius.circular(30)),
                       color: Colors.white),
                   width: double.infinity,
-                  child: Column(
-                    children: <Widget>[
-                      const SizedBox(
-                        height: 20,
-                      ),
-                      const Text(
-                        "Persönliche Informationen",
-                        style: TextStyle(fontSize: 20),
-                        textAlign: TextAlign.center,
-                      ),
-                      const SizedBox(height: 20),
-                      Text("Name: ${state.profile?.name}"),
-                      const SizedBox(height: 20),
-                      Text("Geburtsland: ${state.profile?.origin}"),
-                      const SizedBox(height: 20),
-                      Text(
-                          "Berechtigungsrolle: ${profileCubit.roleTypeFilter(state.profile?.role)}")
-                    ],
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Column(
+                      children: <Widget>[
+                        const SizedBox(
+                          height: 20,
+                        ),
+                        const SizedBox(
+                          height: 30,
+                        ),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Geburtsland: ${state.profile?.origin}",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2,
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Geburtstag: 01.04.2020",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                            textAlign: TextAlign.start,
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2,
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Geburtsort: Berlin",
+                            style: TextStyle(
+                              fontSize: 15,
+                            ),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2,
+                        ),
+                        const SizedBox(height: 20),
+                        Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text(
+                            "Berechtigungsrolle: ${profileCubit.roleTypeFilter(state.profile?.role)}",
+                            style: TextStyle(fontSize: 15),
+                            textAlign: TextAlign.left,
+                          ),
+                        ),
+                        Divider(
+                          thickness: 2,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ]),
---
+++ lib/components/storage/bloc/unit_list_cubit_item.dart
@@ -0,0 +1,109 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../repo/unit.dart';
+
+final List<TileModel> _initialList = [
+  TileModel(
+      description: 'Schraube M8x35mm ',
+      number: 31412,
+      imagePath: "assets/img/storage/storage_image_1.jpeg"),
+  TileModel(
+      description: 'Unterlegscheiben DIN 125 Standard',
+      number: 23232,
+      imagePath: "assets/img/storage/storage_image_2.jpeg"),
+  TileModel(
+      description: 'Unterlegscheiben DIN 440 Standard',
+      number: 33213,
+      imagePath: "assets/img/storage/storage_image_3.jpeg"),
+  TileModel(
+      description: 'Zahnrad',
+      number: 45343,
+      imagePath: "assets/img/storage/storage_image_4.jpeg"),
+  TileModel(
+      description: 'Sme-Schrauben-Element',
+      number: 86096,
+      imagePath: "assets/img/storage/storage_image_5.jpeg"),
+  TileModel(
+      description: 'Befestigungsschiene',
+      number: 96946,
+      imagePath: "assets/img/storage/storage_image_6.jpeg"),
+  TileModel(
+      description: 'Kupplung, 16-32 mm',
+      number: 89471,
+      imagePath: "assets/img/storage/storage_image_7.jpeg"),
+  TileModel(
+      description: 'M8 Hülsen Spreizfunktion',
+      number: 73428,
+      imagePath: "assets/img/storage/storage_image_8.jpeg"),
+  TileModel(
+      description: 'Metallplatte mit Löchern 148x105mm',
+      number: 93812,
+      imagePath: "assets/img/storage/storage_image_9.jpeg"),
+  TileModel(
+      description: 'Metallverbundrohr PE-RT 16x2.0 5m_Stange',
+      number: 98316,
+      imagePath: "assets/img/storage/storage_image_10.jpeg"),
+  TileModel(
+      description: 'Press-Übergangsnippel',
+      number: 97457,
+      imagePath: "assets/img/storage/storage_image_11.jpeg"),
+  TileModel(
+      description: 'RAVENOL Super HD 30 Öl',
+      number: 83284,
+      imagePath: "assets/img/storage/storage_image_12.jpeg"),
+];
+
+final List<TileModel> _idList = [
+  TileModel(
+      description: 'Kupplung, 16-32 mm',
+      number: 89471,
+      imagePath: "assets/img/storage/storage_image_7.jpeg"),
+  TileModel(
+      description: 'M8 Hülsen Spreizfunktion',
+      number: 73428,
+      imagePath: "assets/img/storage/storage_image_8.jpeg"),
+  TileModel(
+      description: 'Metallplatte mit Löchern 148x105mm',
+      number: 93812,
+      imagePath: "assets/img/storage/storage_image_9.jpeg"),
+  TileModel(
+      description: 'Metallverbundrohr PE-RT 16x2.0 5m_Stange',
+      number: 98316,
+      imagePath: "assets/img/storage/storage_image_10.jpeg"),
+  TileModel(
+      description: 'Press-Übergangsnippel',
+      number: 97457,
+      imagePath: "assets/img/storage/storage_image_11.jpeg"),
+  TileModel(
+      description: 'RAVENOL Super HD 30 Öl',
+      number: 83284,
+      imagePath: "assets/img/storage/storage_image_12.jpeg"),
+];
+
+class TileListCubit extends Cubit<List<TileModel>> {
+  TileListCubit() : super(_idList);
+
+  void searchTiles(String query) {
+    if (query.isEmpty) {
+      emit(_initialList);
+    } else {
+      final filteredList = _initialList
+          .where((tile) =>
+              tile.description.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+      emit(filteredList);
+    }
+  }
+
+  void searchIdTiles(String query) {
+    if (query.isEmpty) {
+      emit(_idList);
+    } else {
+      final filteredList = _idList
+          .where((tile) =>
+              tile.description.toLowerCase().contains(query.toLowerCase()))
+          .toList();
+      emit(filteredList);
+    }
+  }
+}
